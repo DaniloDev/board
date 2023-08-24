c2557e426d9e6f0148550f58ed9fb48be63bcacf
Danilo Santos Nascimento <danisn2010@gmail.com>
2023-08-24 19:24:56 -0300

    feat: estilização da home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,25 @@ export default function Home() {
       <Head>
         <title>Board - Organizando suas tarefas.</title>
       </Head>
-      <div>
-        <h1 className={styles.title}>
-          Primeiro projeto com <span>NextJS</span>
-        </h1>
-      </div>
+      <main className={styles.contentContainer}>
+        <img src="/images/board-user.svg" alt="Ferramenta board" />
+
+        <section className={styles.callToAction}>
+          <h1>
+            Uma ferramenta para seu dia a dia Escreva, planeje e organize-se..
+          </h1>
+          <p>
+            <span>100% Gratuita </span>e online.
+          </p>
+        </section>
+
+        <div className={styles.donaters}>
+          <img
+            src="https://media.licdn.com/dms/image/D4E35AQExHETfBQT8ZA/profile-framedphoto-shrink_200_200/0/1692739618899?e=1693519200&v=beta&t=mj-COWZCR0GeWeWf-FEFcmmj0GndwEIlgLg_fG55uSU"
+            alt="Usuario apoaidor"
+          />
+        </div>
+      </main>
     </>
   );
 }
