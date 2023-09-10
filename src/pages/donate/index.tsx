731eadc6b33b62e3ee1dbe5ddb30837536f53dda
Danilo Santos Nascimento <danisn2010@gmail.com>
2023-09-10 20:09:48 -0300

    feat: page support(donate)

--- src/pages/donate/index.tsx
+++ src/pages/donate/index.tsx
@@ -1,9 +1,64 @@
 import styles from "./styles.module.scss";
+import Head from "next/head";
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
 
-export default function Donate() {
+interface DonateProps {
+  user: {
+    id: string;
+    nome: string;
+    image: string;
+  };
+}
+
+export default function Donate({ user }: DonateProps) {
   return (
-    <div>
-      <h1>Página de Donate</h1>
-    </div>
+    <>
+      <Head>
+        <title>Ajude a plataforma board ficar online!</title>
+      </Head>
+      <main className={styles.container}>
+        <img src="/images/rocket.svg" alt="Seja Apoiador" />
+
+        <div className={styles.vip}>
+          <img src={user.image} alt="Imagem Apoiador" />
+          <span>Parabéns você é um novo apoaidor</span>
+        </div>
+
+        <h1>Seja um apoaidor deste projeto 🏆</h1>
+        <h3>
+          Contribua com apenas <span>R$ 1,00</span>
+        </h3>
+        <strong>
+          Apareça na nossa home, tenha funcionalidades exclusivas.
+        </strong>
+      </main>
+    </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+
+  console.log("SESSION ", session);
+
+  if (!session?.id) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  const user = {
+    id: session?.id,
+    nome: session?.user.name,
+    image: session?.user.image,
+  };
+  return {
+    props: {
+      user,
+    },
+  };
+};
